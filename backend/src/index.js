7ba3b6ab1391fac3f7b0fee9c7b7313519c2c1c1
Angelina-Hryachko <angelina.hryachko@gmail.com>
2025-07-20 21:55:19 +0300

    update files

--- backend/src/index.js
+++ backend/src/index.js
@@ -87,9 +87,9 @@ console.log("Mounting frontend fallback")
 
 if (process.env.NODE_ENV === "production") {
 	app.use(express.static(path.join(__dirname, "../frontend/dist")));
-	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
-	});
+	// app.get("*", (req, res) => {
+	// 	res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
+	// })
 }
 
 app.use((err, req, res, next) => {
